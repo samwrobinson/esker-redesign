f1bc913861ad321ca37b548f5a08c4e7a639e013
Sam Robinson <samrobinson@Sams-Mac-mini.home.local>
2025-01-17 20:02:02 -0500

    added new hampshire and pricing

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -98,3 +98,30 @@ document.addEventListener('DOMContentLoaded', handleResize);
 
 // Run on window resize
 window.addEventListener('resize', handleResize);
+
+const leftOption = document.querySelector("#pricing-1262 #cs-option1-1262");
+        const rightOption = document.querySelector("#pricing-1262 #cs-option2-1262");
+        const toggle = document.querySelector("#pricing-1262 .cs-toggle");
+        const cardGroup = Array.from(document.querySelectorAll('#pricing-1262 .cs-ul-wrapper'))
+        // when you click the middle toggle
+        toggle.addEventListener('click', (e) => { 
+            for (const item of cardGroup) {
+                item.classList.toggle("cs-active");
+            }
+            toggle.classList.toggle("active");
+        });       
+        // when you click the left button option
+        leftOption.addEventListener('click', (e) => { 
+            for (const item of cardGroup) {
+                item.classList.remove("cs-active");
+            }
+            toggle.classList.remove("active");
+        });    
+        // when you click the right button option
+        rightOption.addEventListener('click', (e) => { 
+            for (const item of cardGroup) {
+                item.classList.add("cs-active");
+            }
+            toggle.classList.add("active");
+        });
+                                
